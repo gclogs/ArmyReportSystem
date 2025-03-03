3d772aae0797635731541eb63cf7d7cb23aebf1a
gclogs <woig9874@naver.com>
2025-03-03 21:35:41 +0900

    fix(style): 높이 프로퍼티 제거

--- front/src/layouts/Layout.tsx
+++ front/src/layouts/Layout.tsx
@@ -5,13 +5,17 @@ import Header from '../components/molecules/Header';
 import BottomNavigation from '../components/molecules/BottomNavigation';
 
 const LayoutContainer = styled.div`
-  min-height: 100vh;
+  width: 100%;
   background-color: #ffffff;
 `;
 
 const Main = styled.main`
   padding: 64px 0 56px;
   min-height: calc(100vh - 120px);
+
+  @media (max-width: 600px) {
+    padding: 56px 0 48px;
+  }
 `;
 
 const Layout: React.FC = () => {
