c3492a0453e283526fc4009aa8d6216983ce4705
GyeongChan <woig9874@naver.com>
2025-05-10 00:12:09 +0900

    fix: MyBatis Mapper 인터페이스에 findAllReports() 메소드에 대한 SQL 구문 정의

--- back/src/main/java/com/gclogs/armyreportsystem/mapper/ReportMapper.java
+++ back/src/main/java/com/gclogs/armyreportsystem/mapper/ReportMapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.gclogs.armyreportsystem.domain.Report;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ReportMapper {
     List<Report> findAllReports();
