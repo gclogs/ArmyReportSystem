80982170c5c9338c9e5d9cdbd558ba2e93cba573
GyeongChan <woig9874@naver.com>
2025-05-15 02:37:30 +0900

    feat: JWT 인증 필터 구현

---
+++ back/src/main/java/com/gclogs/armyreportsystem/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,37 @@
+package com.gclogs.armyreportsystem.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+
+        if (jwtTokenProvider.isBlacklistToken(token)) {
+            throw new RuntimeException("블랙리스트에 등록된 토큰입니다.");
+        }
+        
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        
+        chain.doFilter(request, response);
+    }
+}
