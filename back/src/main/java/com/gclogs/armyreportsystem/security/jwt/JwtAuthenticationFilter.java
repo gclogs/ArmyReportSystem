0372d26c4b70e391d1015147e1b6fdb5d96cb490
GyeongChan <woig9874@naver.com>
2025-05-21 08:54:40 +0900

    feat: jwt 토큰 검증 로직 추가

--- back/src/main/java/com/gclogs/armyreportsystem/security/jwt/JwtAuthenticationFilter.java
+++ back/src/main/java/com/gclogs/armyreportsystem/security/jwt/JwtAuthenticationFilter.java
@@ -1,10 +1,12 @@
 package com.gclogs.armyreportsystem.security.jwt;
 
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -23,12 +25,17 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
 
+        if (token == null) {
+            throw new RuntimeException("토큰이 없습니다.");
+        }
+
         if (jwtTokenProvider.isBlacklistToken(token)) {
             throw new RuntimeException("블랙리스트에 등록된 토큰입니다.");
         }
         
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+        if (jwtTokenProvider.validateToken(token)) {
+            Claims userId = jwtTokenProvider.getUserIdFromToken(token);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userId, null, null);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         
