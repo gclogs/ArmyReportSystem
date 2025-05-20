e933c7380155d571951677969420a090da89f653
GyeongChan <woig9874@naver.com>
2025-05-21 08:52:35 +0900

    feat: token 갱신 엔드포인트  구현

--- back/src/main/java/com/gclogs/armyreportsystem/controller/AuthController.java
+++ back/src/main/java/com/gclogs/armyreportsystem/controller/AuthController.java
@@ -4,9 +4,14 @@ import com.gclogs.armyreportsystem.dto.LoginRequest;
 import com.gclogs.armyreportsystem.dto.LoginResponse;
 import com.gclogs.armyreportsystem.dto.RegisterRequest;
 import com.gclogs.armyreportsystem.dto.RegisterResponse;
+import com.gclogs.armyreportsystem.security.jwt.TokenRequest;
+import com.gclogs.armyreportsystem.security.jwt.TokenResponse;
+import com.gclogs.armyreportsystem.security.jwt.TokenService;
 import com.gclogs.armyreportsystem.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.Token;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
+    private final TokenService tokenService;
 
     @PostMapping("/register")
     public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request, HttpServletRequest httpRequest) {
@@ -26,8 +32,19 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
-        LoginResponse loginData = userService.login(request);
-        return ResponseEntity.ok(loginData);
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
+        TokenResponse tokenData = tokenService.createTokens(request.getUserId());
+        return ResponseEntity.ok(tokenData);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<TokenResponse> refresh(@RequestBody TokenRequest request) {
+        TokenResponse tokenData = tokenService.refreshAccessToken(request.getRefreshToken());
+
+        if (!tokenData.isSuccess()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(tokenData);
+        }
+
+        return ResponseEntity.ok(tokenData);
     }
 }
