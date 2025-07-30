1b20f9cf7ba18a2cd434b32475278192b82a23af
gclogs <woig9874@naver.com>
2025-07-31 00:12:26 +0900

    refactor: 모든 매퍼 가져오기

--- back/src/main/java/com/gclogs/armyreportsystem/ArmyReportSystemApplication.java
+++ back/src/main/java/com/gclogs/armyreportsystem/ArmyReportSystemApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.gclogs.armyreportsystem.security.jwt")
+@MapperScan("com.gclogs.armyreportsystem.*.mapper")
 @SpringBootApplication
 public class ArmyReportSystemApplication {
 
